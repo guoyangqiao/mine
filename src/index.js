489fed813282e30bd00e8875fc4e46ae04f2edbc
guoyangqiao <944734785@qq.com>
2018-06-07 15:40:27 +0800

    asd

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import element from 'playgroud'
+import {element} from "./playground";
 
-ReactDOM.render(
-    element,
-    document.getElementById('root')
-);
+ReactDOM.render(element, document.getElementById('root'));
