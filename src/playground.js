98f999be48229e221ad6ec75262f6866136c1654
guoyangqiao <944734785@qq.com>
2018-06-07 17:09:31 +0800

    asd

--- src/playground.js
+++ src/playground.js
@@ -1,45 +1,34 @@
 import React from 'react';
 
-function Avatar(props) {
-    return (
-        <img className="Avatar"
-             src={props.user.avatarUrl}
-             alt={props.user.name}
-        />
+class Clock extends React.Component {
 
-    );
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(() => this.tick(), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
 }
 
-function UserInfo(props) {
-    return (
-        <div className="UserInfo">
-            <Avatar user={props.user}/>
-            <div className="UserInfo-name">
-                {props.user.name}
-            </div>
-        </div>
-    );
-}
-
-function Comment(props) {
-    return (
-        <div className="Comment">
-            <UserInfo user={props.author}/>
-            <div className="Comment-text">
-                {props.text}
-            </div>
-            <div className="Comment-date">
-                {props.date.toLocaleDateString()}
-            </div>
-        </div>
-    );
-}
-
-export const element = <Comment
-    date={new Date()}
-    text={'I hope you enjoy learning React!'}
-    author={{
-        name: 'Hello Kitty',
-        avatarUrl: 'http://placekitten.com/g/64/64'
-    }}
-/>;
+export const element = <Clock/>;
