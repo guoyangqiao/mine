f48bbf60341f1f74cf43be9cda3931a13c6a6722
guoyangqiao <944734785@qq.com>
2018-06-07 14:59:09 +0800

    asd

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import style from "./search.css"
 
 const Index = () => <div>Hello React!</div>;
 
-const Search = () => <div className={style.nameOfYourCSSClass}>Hello Search Component :)</div>;
-
-
-ReactDOM.render(<Search/>, document.getElementById("index"));
+ReactDOM.render(<Index/>, document.getElementById("js"));
--- webpack.config.js
+++ webpack.config.js
@@ -15,24 +15,6 @@ module.exports = {
                     loader: "babel-loader"
                 }
             },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: "style-loader"
-                    },
-                    {
-                        loader: "css-loader",
-                        options: {
-                            modules: true,
-                            importLoaders: 1,
-                            localIdentName: "[name]_[local]_[hash:base64]",
-                            sourceMap: true,
-                            minimize: true
-                        }
-                    },
-                ]
-            }
         ]
     },
 
