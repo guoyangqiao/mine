302f938cf6b61bf65b9b076cd7f5fa4e58c3f2dc
guoyangqiao <944734785@qq.com>
2018-06-07 15:12:17 +0800

    asd

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const picPath = 'https://www.baidu.com/img/xinshouye_1aa582c0b96d57852497a8bbc4345f15.png';
+
 const user = {
     name: 'utah',
     password: 'cwuCCiwxwcantypS'
 };
 
+const ele = (
+    <h1 className='greeting'>
+        hug hug hug
+    </h1>
+);
+
 function formatName(user) {
     return user.name + ':' + user.password
 }
 
-ReactDOM.render(<div>hi, {formatName(user)}</div>, document.getElementById("js"));
+ReactDOM.render(<div>
+    {user.name}
+    {user.password}
+    hi, {formatName(user)}
+    <img src={picPath}/>
+    {ele}
+</div>, document.getElementById("js"));
