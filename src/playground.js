954e00914580378fc85cea9d6a74dc0c0da67666
guoyangqiao <944734785@qq.com>
2018-06-07 15:44:59 +0800

    asd

--- src/playground.js
+++ src/playground.js
@@ -1,1 +1,17 @@
-export const element = 'sda';
+import React from 'react';
+
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
+}
+
+function App() {
+    return (
+        <div>
+            <Welcome name="Sara"/>
+            <Welcome name="Cahal"/>
+            <Welcome name="Edite"/>
+        </div>
+    );
+}
+
+export const element = <App/>;
