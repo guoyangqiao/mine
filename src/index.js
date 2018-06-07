b730accbb6a6504e06cecda464ecf1ed23f3b546
guoyangqiao <944734785@qq.com>
2018-06-07 15:35:45 +0800

    asd

--- src/index.js
+++ src/index.js
@@ -1,27 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import element from 'playgroud'
 
-const picPath = 'https://www.baidu.com/img/xinshouye_1aa582c0b96d57852497a8bbc4345f15.png';
-
-const user = {
-    name: 'utah',
-    password: 'cwuCCiwxwcantypS'
-};
-
-const ele = (
-    <h1 className='greeting'>
-        hug hug hug
-    </h1>
+ReactDOM.render(
+    element,
+    document.getElementById('root')
 );
-
-function formatName(user) {
-    return user.name + ':' + user.password
-}
-
-ReactDOM.render(<div>
-    {user.name}
-    {user.password}
-    hi, {formatName(user)}
-    <img src={picPath}/>
-    {ele}
-</div>, document.getElementById("js"));
