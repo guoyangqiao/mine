6dcb7ddd93076b1160d94715234a82671b348c39
guoyangqiao <944734785@qq.com>
2018-06-08 15:40:43 +0800

    cc

--- src/index.js
+++ src/index.js
@@ -2,29 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.toggle = this.toggle.bind(this);
-        this.state = {
-            isOpen: false
-        };
-    }
-
-    toggle() {
-        this.setState({
-            isOpen: !this.state.isOpen
-        });
-    }
-
-    render() {
-        return (
-            <div class="container-fluid">
-                name
-            </div>
-        );
-    }
-}
-
 ReactDOM.render(<App/>, document.getElementById('root'));
