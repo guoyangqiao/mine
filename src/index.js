999f3e4cb79a550afe32648dbed79ee3f0683399
guoyangqiao <944734785@qq.com>
2018-06-08 16:53:08 +0800

    progress

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import React from 'react';
+import ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.css';
 import './css/cover.css'
+
+
+class Clock extends React.Component {
+    render() {
+        return <h1>{new Date().toString()}</h1>;
+    }
+}
+
+ReactDOM.render(
+    <Clock/>,
+    document.getElementById('root')
+);
