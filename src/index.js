5773625268df829031c1a0d002e719008e2a0152
guoyangqiao <944734785@qq.com>
2018-06-09 20:59:22 +0800

    progress

--- src/index.js
+++ src/index.js
@@ -2,14 +2,32 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './css/cover.css'
 import ReactDOM from "react-dom";
-import {Clock} from "./js/clock";
 
-function render(id, element) {
-    ReactDOM.render(
-        element,
-        document.getElementById(id)
-    );
 
+class Toggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            toggle: true
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(prevS => ({
+                toggle: !prevS.toggle
+            })
+        )
+    }
+
+    render() {
+        return (<button onClick={this.handleClick}>
+            {this.state.toggle ? "ON" : "OFF"}
+        </button>)
+    }
 }
 
-render('timeclock', <Clock/>);
+ReactDOM.render(
+    <Toggle/>,
+    document.getElementById('root')
+);
